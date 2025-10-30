029c549441eca007b3c0b0d99855fd40ca477b64
itsme-shyam-702 <sssshyam702@gmail.com>
2025-10-30 12:03:13 +0530

    second commit

--- frontend/client/src/api/index.js
+++ frontend/client/src/api/index.js
@@ -1,7 +1,7 @@
 // src/api/index.js
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const BASE_URL = import.meta.env.VITE_API_URL || "https://jr-school-backend-clurk.onrender.com/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
